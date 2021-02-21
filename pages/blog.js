c19dc58cab5b8f3c11a0a3a02bdc716f7d65c7fd
EzequielCaste <ezequiel.caste@gmail.com>
2021-02-20 21:22:05 -0300

    feat: create a blog pages

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,47 @@
-import Layout from '../components/layout'
+import MoreStories from '../components/more-stories';
+import HeroPost from '../components/hero-post';
+import Layout from '../components/layout';
+import { getAllPosts } from '../lib/api';
+import Head from 'next/head';
+import { MAIN_TITLE } from '../lib/constants';
 
-export default function Blog() {
+export default function Index({ allPosts }) {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
-    <Layout>
-      Blog Page
-    </Layout>
-  )
+    <>
+      <Layout>
+        <Head>
+          <title>Blog | {MAIN_TITLE}</title>
+        </Head>
+
+        {heroPost && (
+          <HeroPost
+            title={heroPost.title}
+            coverImage={heroPost.coverImage}
+            date={heroPost.date}
+            author={heroPost.author}
+            slug={heroPost.slug}
+            excerpt={heroPost.excerpt}
+          />
+        )}
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ]);
+
+  return {
+    props: { allPosts },
+  };
 }
