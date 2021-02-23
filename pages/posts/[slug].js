becc8cdd044e4a0147610cc35489fc1406f17189
EzequielCaste <ezequiel.caste@gmail.com>
2021-02-23 18:46:22 -0300

    feat: new styles and rss feed

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -5,6 +5,22 @@ import { getPostBySlug, getAllPosts } from '../../lib/api';
 import Head from 'next/head';
 import { MAIN_TITLE } from '../../lib/constants';
 import markdownToHtml from '../../lib/markdownToHtml';
+import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
+
+const renderers = {
+  image: (image) => {
+    return (
+      <Image
+        src={image.src}
+        alt={image.alt}
+        width={882}
+        height={480}
+        layout="responsive"
+      />
+    );
+  },
+};
 
 export default function Post({ post, morePosts, preview }) {
   const router = useRouter();
@@ -14,17 +30,20 @@ export default function Post({ post, morePosts, preview }) {
   return (
     <Layout preview={preview}>
       <>
-        <article className="mb-32">
+        <article className="prose lg:prose-xl w-2/3 mx-auto mb-32 text-gray-700">
           <Head>
             <title>
               {post.title} | {MAIN_TITLE}
             </title>
             <meta property="og:image" content={post.ogImage.url} />
           </Head>
-          <div
-            className="markdown"
-            dangerouslySetInnerHTML={{ __html: post.content }}
-          />
+          <div className="markdown">
+            <ReactMarkdown
+              className="markdown"
+              children={post.content}
+              renderers={renderers}
+            />
+          </div>
         </article>
       </>
     </Layout>
@@ -45,10 +64,7 @@ export async function getStaticProps({ params }) {
 
   return {
     props: {
-      post: {
-        ...post,
-        content,
-      },
+      post,
     },
   };
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/typography')],
+};
