5a6712db69c87efbd953958d5ff1cdd4f8962556
EzequielCaste <ezequiel.caste@gmail.com>
2021-06-13 19:59:31 -0300

    update project list
    add code and demo links to Project card

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -5,30 +5,32 @@ import Link from 'next/link';
 export const ProjectCard = ({project}) => {
   const {name, image, tags, description, codeLink, demoLink} = project;
   const btn =
-    'text-white px-3 py-2 rounded-md text-sm font-medium bg-gray-900 hover:bg-gray-800 transition-colors';
+    'text-white px-3 py-2 rounded-md text-sm font-medium bg-gray-900 hover:bg-gray-800 transition-colors mr-4';
 
   return (
-    <Link className="my-10" href={demoLink}>
-      <div className="mb-20 bg-gray-700 flex flex-col items-center p-10 rounded shadow-lg transform hover:scale-105 transition-transform cursor-pointer">
-        <Image
-          className="rounded-md"
-          src={`/images/${image}`}
-          width={300}
-          height={200}
-        />
+    <div className="my-10">
+      <div className="h-full mb-10 bg-gray-700 flex flex-col justify-between items-center p-10 rounded shadow-lg transform hover:scale-105 transition-transform cursor-pointer">
+        <Link href={demoLink}>
+          <img
+            className="rounded-md"
+            src={`/images/${image}`}
+            width={300}
+            height={200}
+          />
+        </Link>
         <div className="">
           <div className="font-medium text-2xl my-3">{name}</div>
           <div className="text-md">{description}</div>
-          {/* <div className="flex justify-around mt-5">
+        </div>
+        <div className="mt-5">
           <a className={btn} href={demoLink}>
             Demo
           </a>
           <a className={btn} href={codeLink}>
             Code
           </a>
-        </div> */}
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
