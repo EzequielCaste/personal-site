3e0283a56b6c0e3a983e9a5b3b2dd88cba6b1ef1
EzequielCaste <ezequiel.caste@gmail.com>
2021-08-11 09:37:13 -0300

    make the navbar adjustable to links height
    - link information is now located in link-data.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,27 @@
-import {useState} from 'react';
+import {useState, useRef, useEffect} from 'react';
 import Link from 'next/link';
-import {useRouter} from 'next/router';
+import {links} from '../lib/links-data';
 
 export const Navbar = () => {
   const [menuToggle, setMenuToggle] = useState(false);
-  const router = useRouter();
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
-  const toggleMenu = () => {
-    setMenuToggle(!menuToggle);
-  };
+  const desktopLinks =
+    'text-white px-3 py-2 hover:bg-gray-900 rounded-md text-sm font-medium capitalize ';
 
+  const mobileLinks =
+    'block text-gray-100 px-3 py-2 rounded-md text-base font-medium hover:bg-gray-900 hover:text-white capitalize';
+  const resumeLink = `${mobileLinks} bg-gray-300 text-gray-700`;
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (menuToggle) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [menuToggle]);
   return (
     <nav className="fixed bg-gray-700 w-full shadow-lg z-10">
       <div className="container mx-auto px-2">
@@ -20,7 +32,7 @@ export const Navbar = () => {
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
-              onClick={toggleMenu}
+              onClick={() => setMenuToggle(!menuToggle)}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -58,101 +70,38 @@ export const Navbar = () => {
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="hidden sm:block">
               <div className="flex space-x-4">
-                <Link href="/">
-                  <a
-                    className={`text-white px-3 py-2 hover:bg-gray-900 rounded-md text-sm font-medium ${
-                      router.pathname === '/' && 'hover:bg-gray-900'
-                    }`}
-                  >
-                    Home
-                  </a>
-                </Link>
-                <Link href="/#about">
-                  <a
-                    onClick={toggleMenu}
-                    className="text-white px-3 py-2 hover:bg-gray-900 rounded-md text-sm font-medium"
-                  >
-                    About Me
-                  </a>
-                </Link>
-                <Link href="/#projects">
-                  <a className="text-white px-3 py-2 hover:bg-gray-900 rounded-md text-sm font-medium active:bg-gray-900">
-                    Projects
-                  </a>
-                </Link>
-                <Link href="/posts">
-                  <a
-                    className={`text-white px-3 py-2 hover:bg-gray-900 rounded-md text-sm font-medium  ${
-                      router.pathname.includes('/posts') && 'bg-gray-900'
-                    }`}
-                  >
-                    Blog
-                  </a>
-                </Link>
-                <Link href="mailto:ezequiel.caste@gmail.com">
-                  <a className="text-white px-3 py-2 hover:bg-gray-900 rounded-md text-sm font-medium">
-                    Contact
-                  </a>
-                </Link>
-                <Link href="https://github.com/EzequielCaste/CV/raw/main/Ezequiel%20Castellanos%20Resume.pdf">
-                  <a className="text-gray-900 px-3 py-2 float-right bg-gray-300 rounded-md text-sm font-medium">
-                    Resume
-                  </a>
-                </Link>
+                {links.map((link) => {
+                  const {id, url, text} = link;
+                  return (
+                    <Link key={id} href={url}>
+                      <a className={desktopLinks}>{text}</a>
+                    </Link>
+                  );
+                })}
               </div>
             </div>
           </div>
         </div>
-        <div className="sm:hidden" id="mobile-menu">
-          {menuToggle && (
-            <div className="px-2 pt-2 pb-3 space-y-2">
-              <Link href="/">
-                <a
-                  onClick={toggleMenu}
-                  className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Home
-                </a>
-              </Link>
-              <Link href="/#about">
-                <a
-                  onClick={toggleMenu}
-                  className="text-gray-300 hover:bg-gray-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  About Me
-                </a>
-              </Link>
-              <Link href="#projects">
-                <a
-                  onClick={toggleMenu}
-                  className="text-white px-3 py-2 block hover:bg-gray-900 rounded-md text-sm font-medium"
-                >
-                  Projects
-                </a>
-              </Link>
-              <Link href="/posts">
-                <a
-                  onClick={toggleMenu}
-                  className="text-gray-300 hover:bg-gray-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Blog
-                </a>
-              </Link>
-              <Link href="mailto:ezequiel.caste@gmail.com">
-                <a
-                  onClick={toggleMenu}
-                  className="text-white px-3 py-2 block hover:bg-gray-900 rounded-md text-sm font-medium"
-                >
-                  Contact
-                </a>
-              </Link>
-              <Link href="https://github.com/EzequielCaste/CV/raw/main/Ezequiel%20Castellanos%20Resume.pdf">
-                <a className="block text-gray-900 px-3 py-2 bg-gray-300 rounded-md  text-sm font-medium">
-                  Resume
-                </a>
-              </Link>
-            </div>
-          )}
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          id="mobile-menu"
+        >
+          <div ref={linksRef} className="px-2 pt-2 pb-3 space-y-2">
+            {links.map((link) => {
+              const {id, url, text} = link;
+              return (
+                <Link key={id} href={url}>
+                  <a
+                    onClick={() => setMenuToggle(!menuToggle)}
+                    className={text === 'resume' ? resumeLink : mobileLinks}
+                  >
+                    {text}
+                  </a>
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
     </nav>
